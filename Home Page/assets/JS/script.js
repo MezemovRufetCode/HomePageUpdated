30f2aebd6ad74bd8251766d3b5bb96d43cf4afd8
MezemovRufetCode <90691159+MezemovRufetCode@users.noreply.github.com>
2021-12-10 15:43:52 +0400

    Add files via upload

--- Home Page/assets/JS/script.js
+++ Home Page/assets/JS/script.js
@@ -63,6 +63,11 @@ $(document).ready(function () {
     responsive: {
       0: {
         items: 1,
+        nav: false,
+      },
+      1000: {
+        items: 1,
+        nav: true,
       },
     },
   });
